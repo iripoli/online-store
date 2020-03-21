4efeec33a1884ba8422946e326e6dde850f58bf1
iripoli <iripoli96@gmail.com>
2020-03-21 12:17:27 +0100

    Created homepage component, added styles, and made a hardcoded structure of the home menu

---
+++ src/components/home-page/index.js
@@ -0,0 +1,3 @@
+import HomePage from './view'
+
+export default HomePage
---
+++ src/components/home-page/view.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import './style.scss'
+
+const HomePage = () => {
+
+	return (
+		<div className="homePage">
+			<div className="home-menu">
+				<div className="menu-option">
+					<div className="content">	
+					<h2 className="title">Hats</h2>
+					<p className="subtitle">Shop now</p>
+					</div>
+				</div>
+				<div className="menu-option">
+					<div className="content">	
+					<h2 className="title">Jacket</h2>
+					<p className="subtitle">Shop now</p>
+					</div>
+				</div>
+				<div className="menu-option">
+					<div className="content">	
+					<h2>Snickers</h2>
+					<p>Shop now</p>
+					</div>
+				</div>
+				<div className="menu-option">
+					<div className="content">	
+					<h2>Woman</h2>
+					<p>Shop now</p>
+					</div>
+				</div>
+				<div className="menu-option">
+					<div className="content">	
+					<h2>Men</h2>
+					<p>Shop now</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	)
+
+}
+
+export default HomePage
