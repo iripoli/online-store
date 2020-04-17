c1ccf7e86e41ece1a381782e8a6fdaf983d45298
iripoli <iripoli96@gmail.com>
2020-04-17 22:18:50 +0200

    build unignored

---
+++ build/precache-manifest.f07fa3ca8be617049c4892b74eb1ca79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556020ae27ce737e5de6b2fb16f60ca2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10d69d1b57369cf056e3",
+    "url": "/static/css/main.c05a332c.chunk.css"
+  },
+  {
+    "revision": "e870507a1f3b57b4366b",
+    "url": "/static/js/2.e42d563e.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.e42d563e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d69d1b57369cf056e3",
+    "url": "/static/js/main.c6b61892.chunk.js"
+  },
+  {
+    "revision": "b1f5d24e16eac0624945",
+    "url": "/static/js/runtime-main.e093a1a1.js"
+  },
+  {
+    "revision": "4960206950db12de74b332613af1598b",
+    "url": "/static/media/logo.49602069.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
