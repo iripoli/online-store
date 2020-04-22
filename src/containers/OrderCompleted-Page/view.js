61bb6c54c10ff2571c51387a5110f7b174892949
iripoli <iripoli96@gmail.com>
2020-04-22 14:50:10 +0200

    fixed some responsiveness issues in the complete checkout page and ordercompleted page

--- src/containers/OrderCompleted-Page/view.js
+++ src/containers/OrderCompleted-Page/view.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 import './style.scss'
 
-const OrderCompleted = ()=>{
-  return(
+const OrderCompleted = () => {
+  return (
     <div className="order-completed">
-    <div>
-      YOUR ORDER HAS BEEN COMPLETED SUCCESFULLY!
-    </div>
-    <div>
-      <img alt="people" src={"https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/Hero_Image_Google_Shopping_Blog_Illustration.png"} />
-    </div>
+      <p>
+        YOUR ORDER HAS BEEN COMPLETED SUCCESFULLY!
+    </p>
+      <div>
+        <img alt="people" src={"https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/Hero_Image_Google_Shopping_Blog_Illustration.png"} />
+      </div>
     </div>
   )
 }
